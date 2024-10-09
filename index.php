ade061673beebccd34ff6e8448914a28d2cdd38c
1997kb <71140277+1997kb@users.noreply.github.com>
2024-10-09 10:52:09 +0545

    day3

--- index.php
+++ index.php
@@ -5,34 +5,66 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <style>
-        body{
+        /* body{
             display: flex;
             align-items: center;
             justify-content: center;
             height: 100vh;
 
-        }
+        } */
     </style>
 </head>
 <body>
-    <h1>
-        <?php 
-            $name = "Dark Matter";
-            $read = false;
-            if($read){
-                $message = "You have read $name";
-            }else{
-                $message = "You havent read anything";
-            }          
-        ?>
-        <h1>
-             <?php 
+    <?php
+       $books= [
+        [
+            'name'=>'Book 1',
+            'author'=>'john cena',
+            'releaseYear'=>1968,
+            'URL'=>'johncena@example.com'
+        ],
+        [
+            'name'=>'Book 2',
+            'author'=>'batista',
+            'releaseYear'=>1969,
+            'URL'=>'batista@example.com'
+        ]
 
-            echo $message;
+        ];
+
+        function filterbyAuthor($books,$author){
+            $filteredArray = [];
+            foreach($books as $book){
+                if($book['author'] === $author){
+                    $filteredArray[] = $book;
+                }
+            }
+
+            return $filteredArray;
+        }
+    
+    ?>
+    
+    <h1>
+
+    Recommended books author
             
-            ?>
-            <?= $message ?>
-        </h1>
-    </h1>    
+    </h1>   
+    <ul>
+        <?php foreach(filterbyAuthor($books,"batista") as $book) : ?>
+            <ul>
+                
+                <li>
+                    <a href="<?= $book['URL']; ?>">
+                            <?= $book['name']; ?>
+                    </a> 
+                </li>
+
+            </ul>
+
+        
+        <?php endforeach ?>
+    
+    </ul> 
 </body>
 </html>
